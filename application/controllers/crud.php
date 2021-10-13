d5b1b8c6865d39c7d52fa91bb0dfbda14e85e0ab
Talita Kurnia Morinda <90679089+talitakurniaa@users.noreply.github.com>
2021-10-13 12:20:45 +0700

    CRUD - Belajar menampilkan data dari database menggunakan CI

--- application/controllers/crud.php
+++ application/controllers/crud.php
@@ -3,7 +3,7 @@ class Crud extends CI_Controller
 {
     function __construct()
     {
-        parent::__construct(
+        parent::__construct();
         $this->load->model('m_data');
         $this->load->helper('url');
     }
